0468a7ca601f1c89782820139cbdc3344179ac38
dottiOliviero <oliviero_dotti@hotmail.com>
2024-04-09 10:43:27 +0200

    feat: add frumento beers

--- scripts/populate_db.go
+++ scripts/populate_db.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"beerchampz/pkg/beer"
+	"beerchampz/pkg/common"
 	"beerchampz/pkg/config"
 	"beerchampz/pkg/db"
 	"context"
@@ -11,17 +12,18 @@ import (
 	"os"
 )
 
-func createBeerList(data [][]string) []beer.Beer {
+func createBeerList(data [][]string, family common.Family) []beer.Beer {
 	var beers []beer.Beer
 
 	for i, line := range data {
 		if i > 0 { // omit header line
+			var offset = 25
 			var rec beer.Beer
 			for j, field := range line {
 				if j == 0 {
 					rec.Name = field
 				} else if j == 1 {
-					rec.SubStyle = field
+					rec.Style = field
 				} else if j == 2 {
 					rec.ABV = field
 				} else if j == 3 {
@@ -29,21 +31,22 @@ func createBeerList(data [][]string) []beer.Beer {
 				} else if j == 4 {
 					rec.Brewery = field
 				} else if j == 5 {
-					rec.Style = field
+					rec.Shop = field
 				}
 			}
-			rec.Image = fmt.Sprintf("assets/%d.png", i)
+			rec.Image = fmt.Sprintf("assets/%d.png", i+offset)
 			rec.Score = 0
 			rec.SubStyle = ""
+			rec.Family = family
 			beers = append(beers, rec)
 		}
 	}
 	return beers
 }
 
-func readData() []beer.Beer {
+func readData(family common.Family) []beer.Beer {
 	// open file
-	f, err := os.Open("./scripts/beerz.csv")
+	f, err := os.Open("./scripts/beerz-frumento.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -60,19 +63,14 @@ func readData() []beer.Beer {
 	}
 
 	// convert records to array of structs
-	return createBeerList(data)
+	return createBeerList(data, family)
 }
 
 func main() {
 	conf := config.Get()
 	conn := db.GetDb(conf)
-	beers := readData()
+	beers := readData(common.FRUMENTO)
 	beerRepo := beer.NewRepository(conn)
-	dbBeers, _ := beerRepo.GetAll(context.Background())
-	if len(dbBeers) > 0 {
-		fmt.Printf("Beers already exist\n")
-		return
-	}
 	for _, b := range beers {
 		id, err := beerRepo.InsertBeer(context.Background(), beer.MapRequestToInsertParams(b))
 		if err != nil {
